47c397ae6c824480bbe7b0a62a3ce1809205d41e
Michael Reimann <michael.reimann@epfl.ch>
2019-01-16 10:34:06 +0100

    Now two recompile buttons: One for only the latest version of the document, one also checks out latest versions of all included files

--- glatex/recompile_button.py
+++ glatex/recompile_button.py
@@ -18,25 +18,33 @@ class Recompiler(object):
         self.translators = translators
         self.create()
 
-    def run_recompile(self):
+    def run_figures(self):
+        self.run_recompile(included_files=True)
+
+    def run_recompile(self, included_files=False):
         from glatex import main, read_latest
         out_name, doc_id, out_dir = read_latest()
         main(out_name, doc_id, out_dir, self.config, self.translators,
-             do_refresh=True, do_open=False, make_button=False, write_to=self)
+             do_refresh=True, do_open=False, make_button=False,
+             included_files=included_files, write_to=self)
 
     def create(self):
 
         self.root = tk.Tk()
         self.root.resizable(False, False)
-        self.label = tk.Label(self.root, text="Recompile")
-        self.label.grid(row=0, column=0)
+        self.label_compile = tk.Label(self.root, text="Recompile")
+        self.label_compile.grid(row=0, column=0)
+        self.label_figs = tk.Label(self.root, text="Get figures")
+        self.label_figs.grid(row=0, column=1)
         self.img = tk.PhotoImage(file=os.path.join(os.path.split(__file__)[0], 'img', 'refresh.gif'))
 
         self.button1 = tk.Button(self.root, image=self.img, command=self.run_recompile)
         self.button1.grid(row=1, column=0)
+        self.button2 = tk.Button(self.root, image=self.img, command=self.run_figures)
+        self.button2.grid(row=1, column=1)
 
         self.text = tk.Text(self.root)
-        self.text.grid(row=2, column=0)
+        self.text.grid(row=2, column=0, columnspan=2)
 
         self.root.mainloop()
 
--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def find_scripts():
 
 setup(
     name='glatex',
-    version='0.42',
+    version='0.43',
     install_requires=[],
     packages=find_packages(),
     include_package_data=True,
